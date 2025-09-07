928a0f2571074836699988f8cfda4ca985a7f02b
Mostakim69 <mdmostakimhosen35@gmail.com>
2025-09-07 13:37:20 +0600

    Basic Setup Navbar About Contact page

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main>
+      <h1>about Page</h1>
+      <p>This is the about page of the application.</p>
+    </main>
+  );
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <main>
+      <h1>Contact Page</h1>
+      <p>This is the Contact page of the application.</p>
+    </main>
+  );
+}
