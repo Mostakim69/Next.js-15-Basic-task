f42c7d08efdcaa79a6df53a1aec657f34b717adf
Mostakim69 <mdmostakimhosen35@gmail.com>
2025-09-08 19:36:16 +0600

    Images in Next.js

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      { hostname: "encrypted-tbn0.gstatic.com" },
+    ],
+  },
 };
 
 export default nextConfig;
